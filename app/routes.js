0d20528cebfb32e9afb3ae80bcbe9b31a5d0152d
AliDashti-Cap <ali.dashti@capgemini.com>
2023-01-10 12:15:18 +0000

    Added all pages and content from live calculator
    Added all pages and content from live calculator
    
    Added a route for multiple jobs sub-journey.

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,22 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'more-than-one-job-answer'
+router.post('/more-than-one-job-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var moreThanOneJob = req.session.data['more-than-one-job']
+
+  // Check whether the variable matches a condition
+  if (moreThanOneJob == "yes"){
+    // Send user to next page
+    res.redirect('/5-how-many-jobs');
+    console.log("Yes")
+  } else {
+    // Send user to ineligible page
+    res.redirect('/6-total-earnings');
+    console.log("No")
+  }
+
+})
